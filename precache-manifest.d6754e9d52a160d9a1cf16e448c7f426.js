b9ff07450fa60b535453eaf0d741a6458eaf1f6e
kiminseob <kis6473@naver.com>
2020-05-27 13:28:04 +0900

    Updates

---
+++ precache-manifest.d6754e9d52a160d9a1cf16e448c7f426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309ef8279a74a8510c51e286575087a2",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "79379f612579e6f6d893",
+    "url": "/react_movie_app/static/css/main.1737b2f1.chunk.css"
+  },
+  {
+    "revision": "6737155ae99ca9dcfe84",
+    "url": "/react_movie_app/static/js/2.96701288.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movie_app/static/js/2.96701288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79379f612579e6f6d893",
+    "url": "/react_movie_app/static/js/main.6aeb80a9.chunk.js"
+  },
+  {
+    "revision": "c201603b07dd69cccdee",
+    "url": "/react_movie_app/static/js/runtime-main.e6fb717c.js"
+  }
+]);
